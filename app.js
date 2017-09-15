87528f584c7795bfef5aec70dab5dd4fb4024813
doradora23 <yuio3825@gmail.com>
2017-09-16 03:37:14 +0900

    socket.io

--- app.js
+++ app.js
@@ -1,46 +1,28 @@
+/**
+ * Module dependencies.
+ */
+ 
 var express = require('express');
+var routes = require('./routes');
+var http = require('http');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-
+ 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+ 
+var httpServer =http.createServer(app).listen(8080, function(req,res){
+  console.log('Socket IO server has been started');
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// upgrade http server to socket.io server
+var io = require('socket.io').listen(httpServer);
+ 
+io.sockets.on('connection',function(socket){
+   socket.emit('toclient',{msg:'Welcome !'});
+   socket.on('fromclient',function(data){
+       socket.broadcast.emit('toclient',data); // 자신을 제외하고 다른 클라이언트에게 보냄
+       socket.emit('toclient',data); // 해당 클라이언트에게만 보냄. 다른 클라이언트에 보낼려면?
+       console.log('Message from client :'+data.msg);
+   })
 });
 
 module.exports = app;
