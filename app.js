a245583a54092ccc5693ee8f505037c98c4b25f2
doradora23 <yuio3825@gmail.com>
2017-09-16 13:47:49 +0900

    GET userID

--- app.js
+++ app.js
@@ -21,12 +21,11 @@ var httpServer =http.createServer(app).listen(8080, function(req,res){
 var io = require('socket.io').listen(httpServer);
  
 io.sockets.on('connection',function(socket){
-   socket.emit('toclient',{msg:'Welcome !'});
    socket.on('fromclient',function(data){
        socket.broadcast.emit('toclient',data); // 자신을 제외하고 다른 클라이언트에게 보냄
        socket.emit('toclient',data); // 해당 클라이언트에게만 보냄. 다른 클라이언트에 보낼려면?
        console.log('Message from client :'+data.msg);
-       var testIns = new TestModel({ blockcontent: data.msg });
+       var testIns = new TestModel({ uid: data.uid, blockcontent: data.msg });
        console.log('Message to DB:'+data.msg);
        testIns.save(function(err){
          if (err)
