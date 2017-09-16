94905266ae8e9640265f3e142c0f9650caa9aeb0
doradora23 <yuio3825@gmail.com>
2017-09-17 07:21:09 +0900

    chat finish

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var mongoose = require('mongoose');
 var BlockSchema= mongoose.Schema({_id:String, xml_text:String});
 var TestModel = mongoose.model("TestModel", BlockSchema);
 
+var ChatSchema= mongoose.Schema({uid:String, msg:String, room:String},{colleciont:'chat'});
+var ChatModel = mongoose.model("ChatModel", ChatSchema);
+
 var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
  
@@ -28,7 +31,18 @@ io.sockets.on('connection',function(socket){
             socket.emit('toclient',val); // 해당 클라이언트에게 전달
       }
     }
+    }
+    });
+    ChatModel.find(function(err, models){
+      if(err) 
+        return console.error(err);
+      else{
+        for(var val of models) {
+          if(val.room == data._id) {// db의 모든 데이터와 클라이언트의 방을 비교함
+            socket.emit('toclientChat',val); // 해당 클라이언트에게 전달
       }
+    }
+    }
     });
   })
   
@@ -45,6 +59,16 @@ io.sockets.on('connection',function(socket){
     return console.log(err);}
   ) 
   })
+  socket.on('fromclientChat',function(data){
+    console.log(data);
+    socket.broadcast.emit('toclientChat',data); // 자신을 제외하고 다른 클라이언트에게 보냄
+    socket.emit('toclientChat',data); // 자신을 제외하고 다른 클라이언트에게 보냄
+    var testInsChat = new ChatModel({uid:data.uid, msg:data.msg, room:data.room});
+    testInsChat.save(function(err){
+      if (err)
+      return console.log(err);}
+    ) 
+    })
 });
 
 mongoose.connect('mongodb://dreamtree:dreamtreepw@ds135594.mlab.com:35594/cocoding');
