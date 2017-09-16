9aeda822f4172bfff338c6c65beb44ed8a46622b
doradora23 <yuio3825@gmail.com>
2017-09-16 12:09:48 +0900

    mongodb

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ var express = require('express');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
- 
+var mongoose   = require('mongoose');
+
+var BlockSchema= mongoose.Schema({blockcontent:String});
+var TestModel = mongoose.model("TestModel", BlockSchema);
+
 var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
  
@@ -22,7 +26,19 @@ io.sockets.on('connection',function(socket){
        socket.broadcast.emit('toclient',data); // 자신을 제외하고 다른 클라이언트에게 보냄
        socket.emit('toclient',data); // 해당 클라이언트에게만 보냄. 다른 클라이언트에 보낼려면?
        console.log('Message from client :'+data.msg);
+       var testIns = new TestModel({ blockcontent: data.msg });
+       console.log('Message to DB:'+data.msg);
+       testIns.save(function(err){
+         if (err)
+         return console.log(err);
+       })
+       
+      
    })
 });
 
+mongoose.connect('mongodb://<user>:<pw>@ds135594.mlab.com:35594/cocoding');
+mongoose.connection.on('error', console.log);
+
 module.exports = app;
+
